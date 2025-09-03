20eb623a5a97a2a516ff9a3834a20a77e14c42c8
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-09-03 12:51:53 +0200

    Auth Backend: travel journal api

--- 02-travel-journal-api-korrektur/db/index.js
+++ 02-travel-journal-api-korrektur/db/index.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
 try {
-  const client = await mongoose.connect(process.env.MONGO_URI);
-  console.log(`Connected to MongoDB @ ${client.connection.host}`);
+  const client = await mongoose.connect(process.env.MONGO_URI, { dbName: 'travel-journal' });
+  console.log(`Connected to --> ${client.connection.name} <-- MongoDB @ ${client.connection.host}`);
 } catch (error) {
   console.log(error);
   process.exit(1);
